25b80956e806fa66ebc27ee2fd7644ae32f11c0c
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-04-22 17:51:36 +0000

    2023/04/22 (pm)

---
+++ Section04-IntelliJ Basics/if-then_Statement-34/src/Hello_ifThen.java
@@ -0,0 +1,9 @@
+public class Hello_ifThen {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+		boolean isAlien = false;
+		if(isAlien == true)  // Inserting a semicolon after the 'if statement', it will still run. Don't do it.
+			System.out.println("It is not an alien!");
+	}
+}
---
+++ Section04-IntelliJ Basics/if-then_Statement-34/src/Hello_ifThenWithCode_block.java
@@ -0,0 +1,13 @@
+public class Hello_ifThenWithCode_block {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+		boolean isAlien = true;
+		if(isAlien == false) {
+			System.out.println("It is not an alien!");
+			System.out.println("It's an alien!");
+		}
+	}
+}
+
+// Without the Code block, both if statements will run.
