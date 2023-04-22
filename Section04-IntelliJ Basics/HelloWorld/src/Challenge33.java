08ea10565948f92c8449b2a026e2c40a11d2ff6b
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-04-22 17:01:56 +0000

    2023/04/12 (pm) Section03. Primitive data types and Strings.

---
+++ Section04-IntelliJ Basics/HelloWorld/src/Challenge33.java
@@ -0,0 +1,11 @@
+public class Challenge33 {
+	public static void main(String[] args) {
+
+		String String1 = "Hmm,I could have be cocky because you haven't a clue how ThreeJS is written...\n";
+		String String2 = "And I get it,ask any other Developer to try writing it ALL in an HTML file.\n\t";
+		String String3 = "Give me some credit or at least some respect for being able to do that!\n\t\t";
+		String String4 = "You should see what I'm capable with 'React-three-fibre',a.k.a. R3f...\n\t\t\t";
+
+		System.out.println(String1 + " " + String2 + " " + String3 + " " + String4);
+	}
+}
