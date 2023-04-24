0f74a502f6d2b7eacb3cf866689530ed6c061684
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-04-24 04:55:57 +0000

    2023/04/23 (pm) Practice.

---
+++ Practice_JavaAdvanced/src/jShell_V1.java
@@ -0,0 +1,13 @@
+public class jShell_V1 {
+	public static void main(String[] args) {
+		/*
+   ---> jShell_V1 Defined:
+
+>>> Runs with CMD after installing jShell_V1.
+>>> EXmp. => jshell> .../...
+		 */
+
+		String jShellDemo1 = "jshell> int aValue = 1";
+				System.out.print("Value of int is = " + jShellDemo1);
+	}
+}
---
+++ Practice_JavaAdvanced/src/jShell_V2.java
@@ -0,0 +1,12 @@
+public class jShell_V2 {
+	public static void main(String[] args) {
+		// Data types:
+		int dataType1 = 5;
+		byte dataType2 = 1;
+		double dataType3 = 10.00d;
+		float dataType4 = 15.00F;
+		long dataType5 = 20000L;
+
+
+	}
+}
