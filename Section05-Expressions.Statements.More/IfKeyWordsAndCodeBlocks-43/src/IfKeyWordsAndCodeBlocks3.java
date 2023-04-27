0d171101c2780293be4a521cf844738757a63549
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-04-27 19:20:49 +0000

    2023/04/26 (am) Section05-43: If & Code Blocks.

---
+++ Section05-Expressions.Statements.More/IfKeyWordsAndCodeBlocks-43/src/IfKeyWordsAndCodeBlocks3.java
@@ -0,0 +1,15 @@
+public class IfKeyWordsAndCodeBlocks3 {
+	public static void main(String[] args) {
+
+		boolean gameOver = true;
+		int score = 5000;
+		int levelCompleted = 5;
+		int bonus = 100;
+
+		if (score <= 5000) {
+			System.out.println("Your score was less than or 5000.");
+		} else {
+			System.out.println("Got there!");
+		}
+	}
+}
