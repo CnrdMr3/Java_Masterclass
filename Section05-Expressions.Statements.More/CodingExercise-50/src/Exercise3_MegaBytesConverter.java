c6403766dc9b5b742554e1ca81733ae67962fd77
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-05-18 11:42:24 +0000

    2023/05/18

--- Section05-Expressions.Statements.More/CodingExercise-50/src/Exercise3_MegaBytesConverter.java
+++ Section05-Expressions.Statements.More/CodingExercise-50/src/Exercise3_MegaBytesConverter.java
@@ -1,2 +1,12 @@
 public class Exercise3_MegaBytesConverter {
+	public static void printMegaBytesAndKiloBytes (int kiloBytes){
+		if(kiloBytes <0){
+			System.out.println("Invalid Value");
+		}else{
+			int convertBytes = kiloBytes;
+			int megaByte = (convertBytes / 1024);
+			int remainingkiloByte = (convertBytes % 1024);
+			System.out.println(convertBytes + " KB = " + megaByte + " MB and " + remainingkiloByte + " KB");
+		}
+	}
 }
